4efb44fd95274e52bb9bab71104d4af6b6e189f6
admbmg <bmg@au.dk>
2025-11-20 22:59:02 +0100

    Add .NET sample API with Argo CD deployment

---
+++ dotnet-api/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Ok(new { message = "Hello from .NET 8 on AKS via Argo CD" }));
+app.MapGet("/healthz", () => Results.Ok("ok"));
+app.MapGet("/version", () => Results.Ok(new { version = "v1" }));
+
+app.Run();
